874b3f35e9519da03da0331a2e5136f08724a735
Rajat Kumar <42904172+rajatkr83@users.noreply.github.com>
2021-05-04 12:13:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
     backgroundColor: Colors.red.shade700,
     appBar: AppBar(
